f5e60f3bf475a9fb0f6584a8e05964f7279b1a7d
Jordan Singer <singer.jordan@gmail.com>
2023-09-10 18:21:16 -0400

    Delete dist directory

--- dist/components/Button.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-declare const Button: ({ variant, }: {
-    variant: "primary" | "secondary";
-}) => JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import Button from "./components/Button";
-export { Button };
